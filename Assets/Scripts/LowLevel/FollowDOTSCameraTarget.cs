2a0fc39dbe0cb39e47ae2aba5de497946e13b423
Patryk Pastuszka <nov11157@gmail.com>
2024-01-30 19:56:34 +0100

    Frog camera follower

---
+++ Assets/Scripts/LowLevel/FollowDOTSCameraTarget.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+
+public class FollowDOTSCameraTarget : MonoBehaviour
+{
+    private EntityManager _entityManager;
+    private EntityQuery _cameraTargetTagQuery;
+    private Transform _transform = null;
+
+    private void Awake()
+    {
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        _cameraTargetTagQuery = _entityManager.CreateEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[]
+                {
+                typeof(Assets.Scripts.Player.Components.Player)
+                }
+        });
+    }
+
+    void LateUpdate()
+    {
+        if (_transform == null)
+        {
+            _transform = GetComponent<Transform>();
+        }
+
+        Entity cameraTargetEntity = _cameraTargetTagQuery.GetSingletonEntity();
+
+        if (_entityManager.HasComponent<LocalToWorld>(cameraTargetEntity))
+        {
+            LocalToWorld cameraLocalToWorld = _entityManager.GetComponentData<LocalToWorld>(cameraTargetEntity);
+            _transform.position = cameraLocalToWorld.Position;
+        }
+    }
+}
